23957fd2dda05dcec5b00edc4d6a5670b41d3611
Javon <Nickgo@192.168.2.3>
2012-10-02 18:45:30 +0800

    This's the first commit test.

---
+++ 00_JavaTest/src/info/javon/abalone/ChessBoard.java
@@ -0,0 +1,23 @@
+package info.javon.abalone;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ChessBoard {
+	
+	private static JFrame frame;
+	
+	public static void main(String[] args) {
+		frame =new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(0, 0, 640, 440);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		
+		JPanel jPanel = new JPanel();
+		frame.add(jPanel);
+		
+		
+		
+	}
+}
